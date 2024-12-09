ead606d349d547ecf7749e40f2553394bc7574f4
DESKTOP-0B2V58C\HP <kachur-shefer.davyd@student.uzhnu.edu.ua>
2024-12-09 14:57:08 +0200

    feat: responsive signIn

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 export default function SignInPage() {
   return (
     <div
-      className="relative flex items-center justify-center h-screen bg-cover bg-no-repeat  overflow-hidden py-5"
+      className="relative flex items-center justify-center h-screen bg-cover bg-no-repeat overflow-hidden py-5 sm:px-0 px-5"
       style={{
         backgroundImage: "url('home.jpg')",
         backgroundPosition: "50% 50%",
       }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-40"></div>
-      <div className="relative z-10 bg-white p-8 rounded-3xl border border-black shadow-2xl  w-1/3 pt-8 pb-10 text-black">
+      <div className="relative z-10 bg-white p-8 rounded-3xl border border-black shadow-2xl w-full max-w-lg pt-8 pb-10 text-black">
         <div className="flex mb-6">
           <Link className="text-green-500 hover:underline" href="/">
             Home
@@ -20,7 +20,7 @@ export default function SignInPage() {
           <div>Sign In</div>
         </div>
         <h1 className="text-4xl font-bold mb-4 text-center">Welcome Back</h1>
-        <p className="mt-4  mb-8 text-center">
+        <p className="mt-4 mb-8 text-center">
           Don’t have an account?{" "}
           <a
             href="/signup"
@@ -64,13 +64,15 @@ export default function SignInPage() {
             Reset it here
           </a>
         </p>
-        <div className="w-full flex flex-col items-center">
+        <div className="w-full flex flex-col items-center mt-6">
           <div className="flex items-center justify-center w-full my-4">
             <div className="border-t border-black flex-grow"></div>
             <span className="px-4 text-black text-sm">Or Sign In with</span>
             <div className="border-t border-black flex-grow"></div>
           </div>
-          <button className="bg-white p-1 rounded-xl shadow-md border-r border-b border-gray-400 hover:scale-105"> <img src="/google.svg" alt="google-icon" /></button>
+          <button className="bg-white p-1 rounded-xl shadow-md border-r border-b border-gray-400 hover:scale-105">
+            <img src="/google.svg" alt="google-icon" />
+          </button>
         </div>
       </div>
     </div>
